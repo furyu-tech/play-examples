5bb203b0c81e8cb9983d4c0a9b75babb1a42cd4e
KUOKA Yusuke <kuoka@furyu.jp>
2011-05-11 21:40:13 +0900

    fixed functional test.

--- rest/test/ApplicationTest.java
+++ rest/test/ApplicationTest.java
@@ -1,31 +1,45 @@
-import org.junit.AfterClass;
-import org.junit.Test;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import models.Photo;
 import org.junit.Before;
-import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import play.Play;
-import play.test.*;
-import play.mvc.*;
-import play.mvc.Http.*;
-import models.*;
+import play.db.jpa.Blob;
+import play.db.jpa.JPA;
+import play.mvc.Http.Response;
+import play.test.Fixtures;
+import play.test.FunctionalTest;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ApplicationTest extends FunctionalTest {
+    static File preparedFile;
+    Photo preparedPhoto;
+
+    @BeforeClass
+    public static void prepareFile() {
+        preparedFile = Play.getFile("public/images/favicon.png");
+    }
 
     @Before
-    public void setUp() {
+    public void setUp() throws FileNotFoundException {
         Fixtures.deleteAll();
         Fixtures.load("data.yml");
+        preparePhoto();
+    }
+
+    private void preparePhoto() throws FileNotFoundException {
+        preparedPhoto = new Photo();
+        preparedPhoto.title = "example title";
+        preparedPhoto.data = new Blob();
+        preparedPhoto.data.set(new FileInputStream(preparedFile), "image/png");
+        preparedPhoto.save();
     }
 
     @Test
@@ -39,18 +53,19 @@ public class ApplicationTest extends FunctionalTest {
     @Test
     public void testIndexXml() {
         Response response = GET("/photos.xml");
+        assertIsOk(response);
         assertContentType("text/xml; charset=utf-8", response);
     }
 
     @Test
     public void testIndexJson() {
         Response response = GET("/photos.json");
+        assertIsOk(response);
         assertContentType("application/json; charset=utf-8", response);
     }
 
     @Test
     public void testUpload() throws Exception {
-//        Photo.deleteAll();
         Map<String, String> params = new HashMap<String, String>();
         params.put("photo.title", "title");
 
@@ -58,16 +73,19 @@ public class ApplicationTest extends FunctionalTest {
         File file = Play.getFile("public/images/favicon.png");
         files.put("photo.data", file);
 
-        Response response = POST("/photos", params, files);
-
-        // 副作用の検証
-        Photo uploadedPhoto = Photo.find("title = ?", "title").first();
-        assertNotNull(uploadedPhoto);
-        assertEquals(file, uploadedPhoto.data.getFile());
+        Response response = POST("/photos.json", params, files);
 
         // レスポンスの検証
-        assertContentEquals("", response);
-        assertStatus(302, response);
+        JsonObject responseJson = getJsonObject(response);
+        Long actualPhotoId = Long.parseLong(responseJson.getAsJsonObject("photo").getAsJsonPrimitive("id").getAsString());
+
+        assertContentType("application/json; charset=utf-8", response);
+        assertStatus(200, response);
+
+        // 副作用の検証
+        Photo uploadedPhoto = Photo.findById(actualPhotoId);
+        assertNotNull(uploadedPhoto);
+        assertEquals(file, uploadedPhoto.data.getFile());
         assertEquals("http://localhost/photos/" + uploadedPhoto.getId() + "/data", uploadedPhoto.dataUrl);
     }
 
@@ -77,64 +95,33 @@ public class ApplicationTest extends FunctionalTest {
 
     @Test
     public void testUpdate() {
-        Photo photoToDelete = Photo.<Photo>findAll().get(0);
-
-        Response response = PUT("/photos/" + photoToDelete.getId(), "application/x-www-form-urlencoded", "title=modified");
-
-        // 成功時のレスポンスボディ
-        assertContentEquals("", response);
-
-        // 成功時のレスポンスステータス
+        String newTitle = "modified title";
+        Response response = PUT("/photos/" + preparedPhoto.id, "application/x-www-form-urlencoded", "title=" + newTitle);
         assertStatus(200, response);
     }
 
     @Test
     public void testDelete() {
-        Response response = DELETE("/photos/" + Photo.all().<Photo>first().getId());
-
-        assertContentEquals("", response);
+        Response response = DELETE("/photos/" + preparedPhoto.id);
         assertStatus(200, response);
     }
 
     @Test
     public void testIndex() {
         Response response = GET("/photos?start=1&results=1");
-
         assertStatus(200, response);
     }
 
     @Test
     public void testData() throws Exception {
-        String fileContent = "content";
-
-        Photo.deleteAll();
-
-        uploadFile("title", fileContent);
-
-        Photo photoToGetData = Photo.all().first();
-
-        Response response = GET("/photos/" + photoToGetData.getId() + "/data");
-
-        assertContentEquals(fileContent, response);
+        Response response = GET("/photos/" + preparedPhoto.getId() + "/data");
+        assertEquals(preparedFile.length(), response.out.toByteArray().length);
+        assertContentType("image/png", response);
         assertStatus(200, response);
     }
 
-    // テストのため、一時ファイルをアップロードする
-    // 本当はFixtureですませたいが、PlayがBlobを含むFixtureに未対応のため。
-    private Response uploadFile(String title, String content) throws IOException {
-        Map<String, String> parameters = new HashMap<String, String>();
-        Map<String, File> files = new HashMap<String, File>();
-
-        File tempFile = File.createTempFile("temp", "png");
-        tempFile.deleteOnExit();
-
-        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-        writer.write(content);
-        writer.close();
-
-        parameters.put("photo.title", title);
-        files.put("photo.data", tempFile);
-
-        return POST("/photos", parameters, files);
+    private static JsonObject getJsonObject(Response response) {
+        JsonElement element = new JsonParser().parse(getContent(response));
+        return element.getAsJsonObject();
     }
 }
